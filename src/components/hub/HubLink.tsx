4461eb5c04c1c52f44928c400401b149c808d0c7
orest3781 <orest3781@gmail.com>
2026-06-27 15:39:52 -0400

    feat(hub): GameCard, HubApp landing, HubLink back-link

---
+++ src/components/hub/HubLink.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Link from "next/link";
+
+export default function HubLink() {
+  return (
+    <Link href="/" style={{ display: "inline-block", marginBottom: 12, fontSize: 13, color: "#9b8f78", textDecoration: "none" }}>
+      ‹ County Games
+    </Link>
+  );
+}
